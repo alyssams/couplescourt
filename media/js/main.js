6308f428c1e180365abc19d8fbf30be58ef40698
Alyssa Saucedo <asilla7@gmail.com>
2017-07-09 16:57:11 -0700

    added finder and bio

--- media/js/main.js
+++ media/js/main.js
@@ -23,3 +23,62 @@ $('.carousel').carousel({
   interval: 6000,
   pause: "false"
 });
+
+
+$(".selector a.toggle-list").on("click", function(e){
+    e.preventDefault();
+    $(".state-list").slideToggle();
+});
+
+$(".state-list").on("mousewheel", function(e){
+    e.preventDefault();
+
+    if(e.originalEvent.wheelDelta/30 > 0) {
+        _mouseDir = "up";
+        scrollList();
+    }
+    else{
+        _mouseDir = "down";
+        scrollList();
+    }
+});
+
+$(".state-list a.control").on("click", function(e){
+    e.preventDefault();
+});
+
+var _mouseDown = false;
+var _mouseDir = null;
+$(".state-list a.control").mousedown(function(e){
+    e.preventDefault();
+    _mouseDown = true;
+    _mouseDir = $(this).hasClass("up")?"up":"down";
+    scrollList();
+ })
+.mouseup(function(e){
+     e.preventDefault();
+     _mouseDown = false;
+})
+
+function scrollList()
+{
+    var $states = $(".list-container ul");
+    var top = parseInt($states.css("top"));
+    var spd = 10
+    if (isNaN(top))
+        top = 0;
+    if (_mouseDir == "up")
+    {
+        top += spd;
+    }else{
+        top -= spd;
+    }
+    if (top <= 0 && top > -(parseInt($states.height())-120))
+    {
+        $states.css("top", top);
+    }
+    if (_mouseDown)
+    {
+        setTimeout(scrollList, 50);
+    }
+}
